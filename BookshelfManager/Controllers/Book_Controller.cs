01f5ef615b9f17ea140e3b3b0a7af2f63e8016f7
LiptakReka <liptakr@kkszki.hu>
2025-02-10 09:07:17 +0100

    Get all , and get by id is ready to use

---
+++ BookshelfManager/Controllers/Book_Controller.cs
@@ -0,0 +1,54 @@
+﻿using BookshelfManager.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace BookshelfManager.Controllers
+{
+    [Route("Books")]
+    [ApiController]
+    public class Book_Controller : Controller
+    {
+        private readonly LibraryContext _context;
+        public Book_Controller(LibraryContext context)
+        {
+            _context = context;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<Book>> GetAllBooks()
+        {
+            try
+            {
+
+            
+            return StatusCode(418, await _context.Books.ToListAsync());
+            }
+            catch (Exception ex)
+            {
+
+                return BadRequest(ex.Message);
+            }
+        }
+
+
+
+        [HttpGet("GetBookById{Id}")]
+        public async Task<ActionResult<Book>> GetBookById(int id)
+        {
+            try
+            {
+                var list = await _context.Books.Where(x => x.Id == id).ToListAsync();
+                if (list != null) {
+                    return StatusCode(418, list);
+                }
+                else { BadRequest()}
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
