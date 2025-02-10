f8640e3e8ba619394582cdd84c10fb075f3d9313
LiptakReka <liptakr@kkszki.hu>
2025-02-10 09:25:09 +0100

    dto updated for updates

--- BookshelfManager/Models/dtos.cs
+++ BookshelfManager/Models/dtos.cs
@@ -15,4 +15,17 @@ namespace BookshelfManager.Modelsűusing
         public string? Genre { get; set; }
         public decimal? Price { get; set; }
     }
+
+    public class EditBook
+    {
+        [Required(ErrorMessage = "Nem teheted üressé a címeet!")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Nem teheted üressé a szerzőt!")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Nem lehet üres a kiadási év!")]
+        [Range(typeof(DateTime), "01-01-01", "2025-12-31", ErrorMessage = "A dátumnak 01 és 2025 között kell lennie.")]
+        public int PublishedYear { get; set; }
+        public string? Genre { get; set; }
+        public decimal? Price { get; set; }
+    }
 }
