6c5091fe29f72eddab547c082cd2e851db64c78b
LiptakReka <liptakr@kkszki.hu>
2025-02-10 09:23:40 +0100

    Dto is done for making new books

--- BookshelfManager/Controllers/Book_Controller.cs
+++ BookshelfManager/Controllers/Book_Controller.cs
@@ -63,5 +63,7 @@ namespace BookshelfManager.Controllers
             }
             return BadRequest();
         }
+
+
     }
 }
---
+++ BookshelfManager/Models/dtos.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BookshelfManager.Modelsűusing 
+
+{
+    public class CreateBookDto
+    {
+        [Required(ErrorMessage = "Meg kell adnod egy Címet!")]
+        public string Title { get; set; }
+        [Required(ErrorMessage = "Meg kell adnod egy Szerzőt!")]
+        public string Author { get; set; }
+        [Required(ErrorMessage = "Meg kell adnod egy szerzési évet!")]
+        [Range(typeof(DateTime), "01-01-01", "2025-12-31", ErrorMessage = "A dátumnak 01 és 2025 között kell lennie.")]
+        public int PublishedYear { get; set; }
+        public string? Genre { get; set; }
+        public decimal? Price { get; set; }
+    }
+}
